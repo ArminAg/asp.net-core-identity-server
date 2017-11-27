c15de7241de5d7a8f30e52e5ffdcb524a426e72d
Armin Aganbegovic <armin.ag@outlook.com>
2017-11-27 16:13:51 +0000

    Add ApiResource and Client Configurations

---
+++ asp.net-core-identity-server/Config.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_identity_server
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                // new ApiResource("api1", "My API")
+                new ApiResource("administrationApi", "Administration Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId ="m2mPortal",
+                    // No interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // Secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    // Scopes that client has access to
+                    AllowedScopes = { "administrationApi" }
+                }
+            };
+        }
+    }
+}
