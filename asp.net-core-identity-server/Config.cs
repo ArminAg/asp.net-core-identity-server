8b761696abde9887cf6239ca100a9ecfda8d3154
Armin Aganbegovic <armin.ag@outlook.com>
2017-11-28 09:54:31 +0000

    Add Claims to user profile

--- asp.net-core-identity-server/Config.cs
+++ asp.net-core-identity-server/Config.cs
@@ -83,13 +83,25 @@ namespace asp.net_core_identity_server
                 {
                     SubjectId = "1",
                     Username = "alice",
-                    Password = "password"
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
                 },
                 new TestUser
                 {
                     SubjectId = "2",
                     Username = "bob",
-                    Password = "password"
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
                 }
             };
         }
